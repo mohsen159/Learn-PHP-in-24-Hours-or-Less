266c476e7d479bdfefdcaf6d97f7f6716da56b1c
mohsen159 <108313366+mohsen159@users.noreply.github.com>
2022-10-08 20:53:25 +0100

    chapter 7 last update

--- chapter_7.php
+++ chapter_7.php
@@ -3,7 +3,32 @@
 
 #Chapter 7: Using PHP to Create an Application
 
+    #echo  date('md')==1008 ; 
+  
+
+?>
 
 
+<!--html>
+<head><title>Sign up</title></head> <body>
+<h1>Register Here</h1> <form method=”get” action=”signup.php”>
+<table>
+<tr><td>Email Address:</td> <td><input type='text' name='email
+address'> <td></tr> <tr><td>Name:</td> <td><input type='text'
+name='first_last_name'><td></tr> <tr><td>Desired Password:</td> <td>
+<input type='password' name='desiredpassword'><td></tr> <tr>
+<td colspan ='3'> <input type='submit' name='signup' value='Sign up'/>
+</td>
+</tr>
+</table>
+</form>
 
+</body>
+</html-->
+
+
+<?php
+
+#Securing Your Scripts
+/**this chapter is just cuidnes no coding  */
 ?>
